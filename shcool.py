d587541a21e720f9663669d3218327b2b21d399f
alexx133708 <96842102+alexx133708@users.noreply.github.com>
2023-04-14 22:29:46 +0300

    Update shcool.py

--- shcool.py
+++ shcool.py
@@ -1,26 +1,28 @@
 import csv
 import random
+import datetime
 import uuid
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
 import json
 
+
 res_path = 'E:\\bigdata\\school\\csvfiles\\'
 orig_path = 'E:\\bigdata\\school\\csvfiles_orig\\'
 engine = create_engine(url=f"mysql+pymysql://alex3@192.168.1.75/school", echo=False)
+connection = engine.raw_connection()
 
 
 def generate_students():
     mname_list = []
     fname_list = []
-    with open(orig_path + 'russian_names.csv', 'r', encoding='utf-8') as names:
-        for row in names.read().split('.biz'):
-            if len(row.split(';')) > 1:
-                if row.split(';')[2] == 'М':
-                    mname_list.append(row.split(';')[1])
-                if row.split(';')[2] == 'Ж':
-                    fname_list.append(row.split(';')[1])
+    with open('fsurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            fname_list.append(row.split(' ')[0])
+    with open('msurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            mname_list.append(row.split(' ')[0])
 
     msurname_list = []
     fsurname_list = []
@@ -40,11 +42,11 @@ def generate_students():
     sex_list = ['М', 'Ж']
     result_list = []
     for _ in range(150):
-        is_male = random.choice([True, False])
+        is_male = random.randint(0, 1)
         result_list.append({'GUID': str(uuid.uuid4()),
-                            'name': random.choice(mname_list) if is_male == True else random.choice(fname_list),
-                            'surname': random.choice(msurname_list) if is_male == True else random.choice(fsurname_list),
-                            'sex': random.choice(sex_list),
+                            'name': random.choice(mname_list) if is_male == 1 else random.choice(fname_list),
+                            'surname': random.choice(msurname_list) if is_male == 1 else random.choice(fsurname_list),
+                            'sex': 'М' if is_male == 1 else 'Ж',
                             'age': random.choice(age_list),
                             'clss': random.choice(class_list)})
 
@@ -53,27 +55,34 @@ def generate_students():
         dict_writer = csv.DictWriter(output_csv, keys)
         dict_writer.writeheader()
         dict_writer.writerows(result_list)
-
+    return result_list
 
 def generate_teachers():
-    name_list = []
-    with open(orig_path + 'russian_names.csv', 'r', encoding='utf-8') as names:
-        for row in names.read().split('.biz'):
-            if len(row.split(';')) > 1:
-                name_list.append(row.split(';')[1])
+    mname_list = []
+    fname_list = []
+    with open('fsurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            fname_list.append(row.split(' ')[0])
+    with open('msurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            mname_list.append(row.split(' ')[0])
 
-    surname_list = []
-    with open(orig_path + 'russian_surnames.csv', 'r', encoding='utf-8') as surnames:
-        for row in surnames.read().split('.biz'):
-            if len(row.split(';')) > 1:
-                surname_list.append(row.split(';')[1])
-
+    msurname_list = []
+    fsurname_list = []
+    with open('fsurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            fsurname_list.append(row.split(' ')[1])
+    with open('msurnames.txt', 'r', encoding='utf-8') as surnames:
+        for row in surnames.readlines():
+            msurname_list.append(row.split(' ')[1])
     result_list = []
     subj_list = ['русский язык', 'литература', 'математика', 'иностранный язык', 'история']
     for sub in subj_list:
+        is_male = random.randint(0, 1)
         result_list.append({'GUID': str(uuid.uuid4()),
-                            'name': random.choice(name_list),
-                            'surname': random.choice(surname_list),
+                            'name': random.choice(mname_list) if is_male == 1 else random.choice(fname_list),
+                            'surname': random.choice(msurname_list) if is_male == 1 else random.choice(fsurname_list),
+                            'sex': 'М' if is_male == 1 else 'Ж',
                             'subj': sub})
 
     keys = result_list[0].keys()
@@ -82,17 +91,53 @@ def generate_teachers():
         dict_writer.writeheader()
         dict_writer.writerows(result_list)
 
-
 def generate_subj():
     subj_list = ['русский язык', 'литература', 'математика', 'иностранный язык', 'история']
     with open(res_path + 'subjects.csv', 'w', newline='', encoding='utf-8') as output_csv:
         writer = csv.writer(output_csv, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(subj_list)
+    return subj_list
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
 
 
-generate_students()
+def generate_shedule(students, subj):
+    i = 0
+    result_list = []
+    for date in daterange(start_date= datetime.date(2022, 9, 1), end_date=datetime.date(2023, 5, 31)):
+        for student in students:
+            for _ in range(random.randint(2, 3)):
+                result_list.append({'ID': i,
+                                    'student_id': student["GUID"],
+                                    'date': date,
+                                    'subj': random.choice(subj),
+                                    'rate': random.randint(2, 5)
+                                    })
+                i += 1
+    keys = result_list[0].keys()
+    with open(res_path + 'shedule.csv', 'w', newline='', encoding='utf-8') as output_csv:
+        dict_writer = csv.DictWriter(output_csv, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(result_list)
+
+def generate_calendar():
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS calendar")
+    cursor.execute("CREATE TABLE calendar"
+                   "(date date, weekday smallint)")
+    for single_date in daterange(start_date= datetime.date(2022, 9, 1), end_date=datetime.date(2023, 5, 31)):
+        cursor.execute("INSERT INTO calendar VALUES (%s, %s)", (single_date, single_date.weekday()))
+        connection.commit()
+    print(cursor.fetchall())
+
+
 generate_teachers()
-generate_subj()
+
+generate_calendar()
+generate_shedule(generate_students(), generate_subj())
+
 
 csv_file = pd.read_csv(res_path + 'students.csv', delimiter=',', on_bad_lines='skip')
 csv_file.to_sql("students", engine, if_exists='replace', index=False)
@@ -100,3 +145,5 @@ csv_file = pd.read_csv(res_path + 'teachers.csv', delimiter=',', on_bad_lines='s
 csv_file.to_sql("teachers", engine, if_exists='replace', index=False)
 csv_file = pd.read_csv(res_path + 'subjects.csv', delimiter=',', on_bad_lines='skip')
 csv_file.to_sql("subjects", engine, if_exists='replace', index=False)
+csv_file = pd.read_csv(res_path + 'shedule.csv', delimiter=',', on_bad_lines='skip')
+csv_file.to_sql("shedule", engine, if_ex
